e42ee54982d18b5b21246c90012f395faeb57abb
shalinshukla <skshukla96@gmail.com>
2021-07-07 17:06:03 +0530

    Add the changes for the jimdeteret

---
+++ web/sites/default/files/php/twig/60e591561418e_eck-entity--component--we_xuUA7dfpgVD38q_4mbUNA_KPV/lZB7-t4H5muO_FUHs9kU4osvHgjz3_phB0zpQOoBNrs.php
@@ -0,0 +1,130 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* themes/custom/actadigitech/templates/component/webform/eck-entity--component--webform_and_contact_us.html.twig */
+class __TwigTemplate_0851dc2641e698282a41093b65230283b7c597088aa2f52851a6298d5a9ff176 extends \Twig\Template
+{
+    private $source;
+    private $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+        $this->sandbox = $this->env->getExtension('\Twig\Extension\SandboxExtension');
+        $this->checkSecurity();
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        // line 1
+        $context["body"] = twig_get_attribute($this->env, $this->source, twig_get_attribute($this->env, $this->source, twig_get_attribute($this->env, $this->source, twig_get_attribute($this->env, $this->source, ($context["eck_entity"] ?? null), "field_description", [], "any", false, false, true, 1), "getValue", [], "method", false, false, true, 1), 0, [], "any", false, false, true, 1), "value", [], "any", false, false, true, 1);
+        // line 2
+        echo "
+";
+        // line 3
+        $context["classes"] = [0 => "white"];
+        // line 6
+        echo "
+<section";
+        // line 7
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(twig_get_attribute($this->env, $this->source, twig_get_attribute($this->env, $this->source, ($context["attributes"] ?? null), "addClass", [0 => ($context["classes"] ?? null)], "method", false, false, true, 7), "setAttribute", [0 => "id", 1 => "only_contact_section"], "method", false, false, true, 7), 7, $this->source), "html", null, true);
+        echo ">
+  ";
+        // line 8
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["title_prefix"] ?? null), 8, $this->source), "html", null, true);
+        echo "
+  ";
+        // line 9
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["title_suffix"] ?? null), 9, $this->source), "html", null, true);
+        echo "
+  <div class=\"content_width_plus_padding\">
+    <div class=\"content_width s_inner\">
+      <div class=\"multi_column s_2_columns\">
+        <div class=\"s_left contact_page_text\">
+          ";
+        // line 14
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->renderVar($this->sandbox->ensureToStringAllowed(($context["body"] ?? null), 14, $this->source));
+        echo "
+        </div>
+        <div class=\"s_gutter\">
+          <!-- Gutter for columns -->
+        </div>
+        <div class=\"s_right contact_page_form\">
+          ";
+        // line 20
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(twig_get_attribute($this->env, $this->source, ($context["content"] ?? null), "field_form", [], "any", false, false, true, 20), 20, $this->source), "html", null, true);
+        echo "
+        </div>
+      </div>
+    </div>
+  </div>
+</section>";
+    }
+
+    public function getTemplateName()
+    {
+        return "themes/custom/actadigitech/templates/component/webform/eck-entity--component--webform_and_contact_us.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  74 => 20,  65 => 14,  57 => 9,  53 => 8,  49 => 7,  46 => 6,  44 => 3,  41 => 2,  39 => 1,);
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("", "themes/custom/actadigitech/templates/component/webform/eck-entity--component--webform_and_contact_us.html.twig", "/var/www/html/jimdetert/web/themes/custom/actadigitech/templates/component/webform/eck-entity--component--webform_and_contact_us.html.twig");
+    }
+    
+    public function checkSecurity()
+    {
+        static $tags = array("set" => 1);
+        static $filters = array("escape" => 7, "raw" => 14);
+        static $functions = array();
+
+        try {
+            $this->sandbox->checkSecurity(
+                ['set'],
+                ['escape', 'raw'],
+                []
+            );
+        } catch (SecurityError $e) {
+            $e->setSourceContext($this->source);
+
+            if ($e instanceof SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+    }
+}
---
+++ web/themes/custom/actadigitech/static/js/custom.js
@@ -0,0 +1,358 @@
+// begin: Custom JS
+if (jQuery(window).width() < 768) {
+  jQuery('.item-expanded__arrow').on('click', function(e) {
+    e.preventDefault();
+    jQuery(this).parent('.item-expanded').toggleClass('open');
+    jQuery('.nav-wrap').toggleClass('sub-menu-open');
+  });
+
+  jQuery('.sub-nav').on('click', function(e) {
+    e.stopPropagation();
+  });
+
+  jQuery('.btn-back').on('click', function(e) {
+    e.preventDefault();
+    jQuery('.item-expanded').removeClass('open');
+    jQuery('.nav-wrap').removeClass('sub-menu-open');
+  });
+}
+
+function resize() {
+  if (jQuery(window).width() > 767) {
+    jQuery('body').removeClass('nav-active');
+    jQuery('.item-expanded').removeClass('open');
+    jQuery('.nav-wrap').removeClass('sub-menu-open');
+  }
+}
+
+jQuery(document).ready( function() {
+    jQuery(window).resize(resize);
+    resize();
+});
+
+// end: Custom JS
+
+
+jQuery(function () {
+  initSlickCarouselCourage();
+  initTabs();
+  initAccordionToggle();
+  initReadMoreShow();
+  initTabNavigation();
+});
+
+function initPagination() {
+  jQuery('.article').paginate({
+    perPage: 5, 
+  });
+}
+
+
+
+function initTabNavigation() {
+  let navItem = jQuery('.navigation-item');
+
+  navItem.on('click', function(e){
+    let link = jQuery(this).attr('href');
+    let nextTab = jQuery('.tabset').find(`a[href="jQuery{link}"]`)
+    nextTab.trigger('click');
+    if (jQuery('.tabset-block').length) {
+      jQuery('html, body').animate({
+        scrollTop: jQuery('.tabset-block').offset().top
+      }, 1000);
+    }
+  });
+
+}
+
+//faq list toggling
+function initAccordionToggle() {
+  let question = jQuery('.accordion-block__title');
+  let answer = jQuery('.accordion-block__content')
+  question.on('click', function(){
+    
+    if( jQuery(this).hasClass('open') ) {
+      jQuery(this).siblings(answer).slideUp(300);
+      jQuery(this).removeClass('open')
+    } else {
+      jQuery(this).siblings(answer).slideDown(300);
+      jQuery(this).addClass('open');
+    }
+  });
+}
+
+function initReadMoreShow() {
+  let btn = jQuery('.accordion-block__btn');
+  let text = jQuery('.short-text');
+
+  btn.on('click', function(){
+    jQuery(this).siblings(text).removeClass('short-text');
+    jQuery(this).hide();
+  })
+
+}
+
+
+
+function initSlickCarouselCourage() {
+
+  jQuery('.text-gallery').each(function () {
+    var jQueryholder = jQuery(this);
+    var jQuerysliderFor = jQueryholder.find('.slider-for');
+    var jQuerysliderNav = jQueryholder.find('.slider-nav');
+
+
+    jQuerysliderFor.slick({
+      rows: 0,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      adaptiveHeight: false,
+      arrows: true,
+      prevArrow: jQuery('.text-gallery__content-block .prev'),
+          nextArrow: jQuery('.text-gallery__content-block .next'),
+      fade: true,
+      asNavFor: jQuerysliderNav.length ? jQuerysliderNav : null,
+      speed: 200,
+    });
+
+    jQuerysliderNav.slick({
+      rows: 0,
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      asNavFor: jQuerysliderFor.length ? jQuerysliderFor : null,
+      arrows: true,
+      prevArrow: jQuery('.text-gallery__tabs-block .prev'),
+          nextArrow: jQuery('.text-gallery__tabs-block .next'),
+      dots: false,
+      focusOnSelect: true,
+      infinite: false,
+      draggable: false,
+      swipeToSlide: false,
+      touchMove: false,
+      swipe: false,
+      speed: 200,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+          }
+        },
+        ]
+    });
+  });
+}
+
+// content tabs init
+function initTabs() {
+  jQuery('.tabset').tabset({
+        activeClass: 'active',  // active class for active tab navigation link
+        addToParent: true,    // add the class not to the link but to its parent
+        autoHeight: true,   // smooth changing of the height during tabs switching (first parent node is used)
+        animSpeed: 10,      // animation speed
+        tabLinks: 'a',      // selector of tab navigation links in the tabset
+        event: 'click'    // switch on event "click", "mouseover", etc.
+  });
+}
+
+/*
+ * jQuery Tabs plugin
+ */
+
+;(function(jQuery, jQuerywin) {
+  'use strict';
+
+  function Tabset(jQueryholder, options) {
+    this.jQueryholder = jQueryholder;
+    this.options = options;
+
+    this.init();
+  }
+
+  Tabset.prototype = {
+    init: function() {
+      this.jQuerytabLinks = this.jQueryholder.find(this.options.tabLinks);
+
+      this.setStartActiveIndex();
+      this.setActiveTab();
+
+      if (this.options.autoHeight) {
+        this.jQuerytabHolder = jQuery(this.jQuerytabLinks.eq(0).attr(this.options.attrib)).parent();
+      }
+
+      this.makeCallback('onInit', this);
+    },
+
+    setStartActiveIndex: function() {
+      var jQueryclassTargets = this.getClassTarget(this.jQuerytabLinks);
+      var jQueryactiveLink = jQueryclassTargets.filter('.' + this.options.activeClass);
+      var jQueryhashLink = this.jQuerytabLinks.filter('[' + this.options.attrib + '="' + location.hash + '"]');
+      var activeIndex;
+
+      if (this.options.checkHash && jQueryhashLink.length) {
+        jQueryactiveLink = jQueryhashLink;
+      }
+
+      activeIndex = jQueryclassTargets.index(jQueryactiveLink);
+
+      this.activeTabIndex = this.prevTabIndex = (activeIndex === -1 ? (this.options.defaultTab ? 0 : null) : activeIndex);
+    },
+
+    setActiveTab: function() {
+      var self = this;
+
+      this.jQuerytabLinks.each(function(i, link) {
+        var jQuerylink = jQuery(link);
+        var jQueryclassTarget = self.getClassTarget(jQuerylink);
+        var jQuerytab = jQuery(jQuerylink.attr(self.options.attrib));
+
+        if (i !== self.activeTabIndex) {
+          jQueryclassTarget.removeClass(self.options.activeClass);
+          jQuerytab.addClass(self.options.tabHiddenClass).removeClass(self.options.activeClass);
+        } else {
+          jQueryclassTarget.addClass(self.options.activeClass);
+          jQuerytab.removeClass(self.options.tabHiddenClass).addClass(self.options.activeClass);
+        }
+
+        self.attachTabLink(jQuerylink, i);
+      });
+    },
+
+    attachTabLink: function(jQuerylink, i) {
+      var self = this;
+
+      jQuerylink.on(this.options.event + '.tabset', function(e) {
+        e.preventDefault();
+
+        if (self.activeTabIndex === self.prevTabIndex && self.activeTabIndex !== i) {
+          self.activeTabIndex = i;
+          self.switchTabs();
+        }
+        if (self.options.checkHash) {
+          location.hash = jQuery(this).attr('href').split('#')[1]
+        }
+      });
+    },
+
+    resizeHolder: function(height) {
+      var self = this;
+
+      if (height) {
+        this.jQuerytabHolder.height(height);
+        setTimeout(function() {
+          self.jQuerytabHolder.addClass('transition');
+        }, 10);
+      } else {
+        self.jQuerytabHolder.removeClass('transition').height('');
+      }
+    },
+
+    switchTabs: function() {
+      var self = this;
+
+      var jQueryprevLink = this.jQuerytabLinks.eq(this.prevTabIndex);
+      var jQuerynextLink = this.jQuerytabLinks.eq(this.activeTabIndex);
+
+      var jQueryprevTab = this.getTab(jQueryprevLink);
+      var jQuerynextTab = this.getTab(jQuerynextLink);
+
+      jQueryprevTab.removeClass(this.options.activeClass);
+
+      if (self.haveTabHolder()) {
+        this.resizeHolder(jQueryprevTab.outerHeight());
+      }
+
+      setTimeout(function() {
+        self.getClassTarget(jQueryprevLink).removeClass(self.options.activeClass);
+
+        jQueryprevTab.addClass(self.options.tabHiddenClass);
+        jQuerynextTab.removeClass(self.options.tabHiddenClass).addClass(self.options.activeClass);
+
+        self.getClassTarget(jQuerynextLink).addClass(self.options.activeClass);
+
+        if (self.haveTabHolder()) {
+          self.resizeHolder(jQuerynextTab.outerHeight());
+
+          setTimeout(function() {
+            self.resizeHolder();
+            self.prevTabIndex = self.activeTabIndex;
+            self.makeCallback('onChange', self);
+          }, self.options.animSpeed);
+        } else {
+          self.prevTabIndex = self.activeTabIndex;
+        }
+      }, this.options.autoHeight ? this.options.animSpeed : 1);
+    },
+
+    getClassTarget: function(jQuerylink) {
+      return this.options.addToParent ? jQuerylink.parent() : jQuerylink;
+    },
+
+    getActiveTab: function() {
+      return this.getTab(this.jQuerytabLinks.eq(this.activeTabIndex));
+    },
+
+    getTab: function(jQuerylink) {
+      return jQuery(jQuerylink.attr(this.options.attrib));
+    },
+
+    haveTabHolder: function() {
+      return this.jQuerytabHolder && this.jQuerytabHolder.length;
+    },
+
+    destroy: function() {
+      var self = this;
+
+      this.jQuerytabLinks.off('.tabset').each(function() {
+        var jQuerylink = jQuery(this);
+
+        self.getClassTarget(jQuerylink).removeClass(self.options.activeClass);
+        jQuery(jQuerylink.attr(self.options.attrib)).removeClass(self.options.activeClass + ' ' + self.options.tabHiddenClass);
+      });
+
+      this.jQueryholder.removeData('Tabset');
+    },
+
+    makeCallback: function(name) {
+      if (typeof this.options[name] === 'function') {
+        var args = Array.prototype.slice.call(arguments);
+        args.shift();
+        this.options[name].apply(this, args);
+      }
+    }
+  };
+
+  jQuery.fn.tabset = function(opt) {
+    var args = Array.prototype.slice.call(arguments);
+    var method = args[0];
+
+    var options = jQuery.extend({
+      activeClass: 'active',
+      addToParent: false,
+      autoHeight: false,
+      checkHash: false,
+      defaultTab: true,
+      animSpeed: 500,
+      tabLinks: 'a',
+      attrib: 'href',
+      event: 'click'
+    }, opt);
+    options.autoHeight = options.autoHeight;
+
+    return this.each(function() {
+      var jQueryholder = jQuery(this);
+      var instance = jQueryholder.data('Tabset');
+
+      if (typeof opt === 'object' || typeof opt === 'undefined') {
+        jQueryholder.data('Tabset', new Tabset(jQueryholder, options));
+      } else if (typeof method === 'string' && instance) {
+        if (typeof instance[method] === 'function') {
+          args.shift();
+          instance[method].apply(instance, args);
+        }
+      }
+    });
+  };
+}(jQuery, jQuery(window)));
