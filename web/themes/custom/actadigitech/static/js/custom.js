bcda049250035088f70c15d88b63063e2e6e8821
actadigitech <hey_nik@outlook.com>
2021-08-31 14:01:34 +0530

    css

--- web/themes/custom/actadigitech/static/js/custom.js
+++ web/themes/custom/actadigitech/static/js/custom.js
@@ -32,6 +32,35 @@ jQuery(document).ready( function() {
     jQuery("header #block-socialshare").appendTo("ul.menu.menu--main.nav");
 });
 
+(function () {
+  equalHeight(false);
+})();
+
+window.onresize = function(){
+  equalHeight(true);
+}
+
+function equalHeight(resize) {
+  var elements = document.getElementsByClassName("equalHeight"),
+      allHeights = [],
+      i = 0;
+  if(resize === true){
+    for(i = 0; i < elements.length; i++){
+      elements[i].style.height = 'auto';
+    }
+  }
+  for(i = 0; i < elements.length; i++){
+    var elementHeight = elements[i].clientHeight;
+    allHeights.push(elementHeight);
+  }
+  for(i = 0; i < elements.length; i++){
+    elements[i].style.height = Math.max.apply( Math, allHeights) + 'px';
+    if(resize === false){
+      elements[i].className = elements[i].className + " show";
+    }
+  }
+}
+
 // end: Custom JS
 
 
